007986d77153a5d3b84a3cc39fc9a4b91e86ea95
csc123 <c_schibli@sunrise.ch>
2016-06-07 09:31:01 +0200

    New Version 7.6.16

--- src/main/java/ch/fhnw/ws4c/customControl/HeaderUI.java
+++ src/main/java/ch/fhnw/ws4c/customControl/HeaderUI.java
@@ -1,49 +1,34 @@
 package ch.fhnw.ws4c.customControl;
 
 import javafx.geometry.Insets;
-import javafx.geometry.Pos;
 import javafx.scene.layout.*;
-import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
-import javafx.scene.text.*;
 import javafx.scene.layout.HBox;
 
 /**
  * Created by dimitri on 21.05.2016.
  */
-public class HeaderUI extends VBox {
-    private Region trainRail;
+public class HeaderUI extends HBox {
     private Integer prefWidth = 500;
 
     private PresentationModel model;
+    private ClockUI clock;
+    private DirectionUI directionUI;
+    private TrainRail railShield;
 
     public HeaderUI(PresentationModel _model){
         model = _model;
         initializeControls();
         layoutControls();
-
     }
 
     private void initializeControls() {
-        // Train Rail Number
-        trainRail = new Region();
-        // Adds all CSS Classes to the Region
-        trainRail.getStyleClass().addAll("detailUI","trainRail");
-        trainRail.setMinWidth(88);
-        trainRail.setMinHeight(86);
-        trainRail.setMaxWidth(88);
-        trainRail.setMaxHeight(86);
-
-        // ToDO Text Element für variable Gleis Number.
-
-        // ToDO Text Center (AnkunfstBahnhof)
-
-        // ToDO PLatzhalter 200x200 px left side
-
+        clock = new ClockUI();
+        railShield = new TrainRail();
+        directionUI = new DirectionUI();
     }
 
     private void layoutControls() {
-
         this.setPrefWidth(prefWidth);
         this.setMinWidth(prefWidth);
         this.setMaxWidth(prefWidth+10);
@@ -53,11 +38,6 @@ public class HeaderUI extends VBox {
         this.setPadding(new Insets(10,10,10,10));
         this.setSpacing(18);
 
-
-        // positions of the Train Rail Icon
-        this.setMargin(trainRail, new Insets(10,10,0,0));
-
-       this.getChildren().addAll(trainRail);
-
+        this.getChildren().addAll(clock, directionUI, railShield);
     }
 }
